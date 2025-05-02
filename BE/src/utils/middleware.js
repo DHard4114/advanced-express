b0533a7da10ba290e4cb6e9cbd4fafd9a60cb9b4
Daffa Hardhan <dapahardan@gmail.com>
2025-05-02 14:56:57 +0700

    Smeoga BE bisa

--- BE/src/utils/middleware.js
+++ BE/src/utils/middleware.js
@@ -21,7 +21,7 @@ const securityMiddleware = (app) => {
 // Middleware untuk CORS
 const corsMiddleware = (app) => {
     const corsOptions = {
-        origin: ['https://os.netlabdte.com', 'http://localhost:5173', 'https://advanced-express-zkl7.vercel.app','http://192.168.76.1:5173'],
+        origin: ['https://os.netlabdte.com', 'http://localhost:5173', 'https://advanced-express-zkl7.vercel.app','http://192.168.76.1:5173', 'https://advanced-express-zkl7-git-main-daffa-hardhans-projects.vercel.app'],
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Content-Type', 'Authorization']
     };
